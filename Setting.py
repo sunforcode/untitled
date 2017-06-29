0f0a1ab7fe19de7afaafb122547427bf3bbe466d
sunyongji <363148025@qq.com>
2017-06-29 18:08:23 +0800

    增加了新文件

---
+++ Setting.py
@@ -0,0 +1,4 @@
+unityFilePath = '/Users/CharlyZhang/Desktop/XCode/Unity-iPhone.xcodeproj/project.pbxproj'
+targetFilePath = '/Users/CharlyZhang/Desktop/TestPythonUnity/TestPythonUnity.xcodeproj/project.pbxproj'
+unityName = ''
+targetFileName = ''
